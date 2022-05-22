b55f6752500b18042592ef412299a0761159c7bb
Yader Velasquez <yajosev@gmail.com>
2022-05-21 18:05:34 -0600

    Add PosgresSQL support

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const templateRoot string = "../../ui/html"
@@ -19,6 +22,7 @@ type application struct {
 func main() {
 	// Get variables
 	addr := flag.String("addr", ":4000", "Http network address")
+	dns := flag.String("db-dns", os.Getenv("DB_DNS"), "DNS Database")
 	flag.Parse()
 
 	// Set custom loggers
@@ -30,6 +34,13 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+
+	db, err := openDB(*dns)
+
+	if db != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// Tie some varibles to the application struct so we share data/fuctions
 	// between the package istead of using global variables
@@ -47,4 +58,27 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dns)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// We create a context with a timeout of 5 seconds
+	// to entablish a db pool connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Verify/entablilsh the connection with the database
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
 }
